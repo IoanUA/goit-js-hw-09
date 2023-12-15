91d3016ef91ec2294de9e94c71fd3b921a67da90
Chuguev_Ivan <Chuguev19930612@gmail.com>
2023-12-15 03:19:36 +0200

    done first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const butStart = document.querySelector("[data-start]");
+const butStop = document.querySelector("[data-stop]");
+const body = document.querySelector('body')
 
+butStart.addEventListener('click', onStartClickRandomColor);
+
+function onStartClickRandomColor(){
+  
+  timerId = setInterval(()=>{
+    const randomColorFun = getRandomHexColor()
+    body.style.backgroundColor = randomColorFun;
+        console.log('dawda');
+      }, 1000)
+      butStart.removeEventListener('click', onStartClickRandomColor);
+}
+
+butStop.addEventListener("click", onStopClickRandomColor)
+function onStopClickRandomColor(){
+clearInterval(timerId)
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  
